ba23e0b357ad93c0ef79d4d5833ea957a102e8c9
maki <148695646+MakiSonomura@users.noreply.github.com>
2024-07-04 20:32:45 +0800

    fixed

---
+++ benchmark/test_main.cu
@@ -0,0 +1,13 @@
+
+#include "../util/gpu_t.cuh"
+
+
+int main() {
+    printf("%d\n", cuda_available());
+    int count = 9999;
+    cudaGetDeviceCount(&count);
+    printf("%d\n", count);
+
+    auto prop = gpu_props(0);
+    printf("%s\n", prop.name);
+}
